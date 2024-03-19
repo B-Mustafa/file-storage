e94e2206aed9f99ce8e34ab589e4ddd8c73be538
Mustafa <mustafabhikhpurwala@gmail.com>
2024-03-19 11:25:19 +0530

    fixes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-light-secondary dark:bg-dark-secondary  hover:bg-light-accent dark:hover:bg-dark-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
